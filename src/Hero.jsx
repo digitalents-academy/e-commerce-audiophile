afa2b0950f1a1d97d10a036e8758d726deca6240
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-08 14:13:37 +0200

    desktop version of "hero" banner

---
+++ src/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div id="hero">
+      <div className="flex-container">
+        <p id="label">New product</p>
+        <h2>XX99 Mark II Headphones</h2>
+        <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+        <a href="/">See product</a>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
