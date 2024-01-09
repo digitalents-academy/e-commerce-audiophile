4a0b2dd5081a12d6b7304e966fd4505867be32ab
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-09 13:55:51 +0200

    yx1 earphones banner done on desktop, tablet and mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './index.css'
-// import Hero from './Hero'
+import Hero from './Hero'
 import ZX9Speaker from './ZX9Speaker'
 import ZX7Speaker from './ZX7Speaker'
+import YX1Earphones from './YX1Earphones'
 
 const App = () => {
   return (
     <div>
+      <Hero />
       <ZX9Speaker />
       <ZX7Speaker />
+      <YX1Earphones />
     </div>
   )
 }
---
+++ src/YX1Earphones.jsx
@@ -0,0 +1,14 @@
+const YX1Earphones = () => {
+  return (
+    <div id="earphones-1">
+      <div id="earphone-photo">
+      </div>
+      <div className="flex-container">
+      <h3>YX1 earphones</h3>
+      <a href="/">See product</a>
+      </div>
+    </div>
+  )
+}
+
+export default YX1Earphones
