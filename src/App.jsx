ddd41d54e836c4f815e2a7f9bf72f8787ea10502
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-11 14:44:47 +0200

    add links & icons in a flex container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './index.css'
-import Hero from './components/home/Hero'
-import ZX9Speaker from './components/home/ZX9Speaker'
-import ZX7Speaker from './components/home/ZX7Speaker'
-import YX1Earphones from './components/home/YX1Earphones'
+// import Hero from './components/home/Hero'
+// import ZX9Speaker from './components/home/ZX9Speaker'
+// import ZX7Speaker from './components/home/ZX7Speaker'
+// import YX1Earphones from './components/home/YX1Earphones'
+import Catalog from './components/shared/Catalog'
 
 const App = () => {
   return (
     <div>
-      <Hero />
+      {/* <Hero />
       <ZX9Speaker />
       <ZX7Speaker />
-      <YX1Earphones />
+      <YX1Earphones /> */}
+      <Catalog />
     </div>
   )
 }
---
+++ src/components/shared/Catalog.jsx
@@ -0,0 +1,20 @@
+const Catalog = () => {
+  return (
+    <div id="catalog">
+      <a href="/">
+        <h4>Headphones</h4>
+        <div>Shop <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" /></div>
+      </a>
+      <a href="/">
+        <h4>Speakers</h4>
+        <div>Shop <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" /></div>
+      </a>
+      <a href="/">
+        <h4>Earphones</h4>
+        <div>Shop <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" /></div>
+      </a>
+    </div>
+  )
+};
+
+export default Catalog;
