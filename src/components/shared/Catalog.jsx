cdc8c0377ee80ca26fe6afca30766f1ae3a0bd62
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-12 09:42:27 +0200

    catalog links done in desktop

--- src/components/shared/Catalog.jsx
+++ src/components/shared/Catalog.jsx
@@ -2,16 +2,19 @@ const Catalog = () => {
   return (
     <div id="catalog">
       <a href="/">
+        <img src="../assets/shared/desktop/image-headphones.png" alt="" className="catalog-image" />
         <h4>Headphones</h4>
-        <div>Shop <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" /></div>
+        <div><span>Shop</span> <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" className="catalog-arrow" /></div>
       </a>
       <a href="/">
+        <img src="../assets/shared/desktop/image-speakers.png" alt="" className="catalog-image" />
         <h4>Speakers</h4>
-        <div>Shop <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" /></div>
+        <div><span>Shop</span> <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" className="catalog-arrow" /></div>
       </a>
       <a href="/">
+        <img src="../assets/shared/desktop/image-earphones.png" alt="" className="catalog-image" />
         <h4>Earphones</h4>
-        <div>Shop <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" /></div>
+        <div><span>Shop</span> <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" className="catalog-arrow" /></div>
       </a>
     </div>
   )
