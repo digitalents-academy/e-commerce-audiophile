8dd9f387b4bfac1db1d79ebc3e2cb076e5000c3c
Jessica Rautakangas <rautjes@digitalentshelsinki.internal>
2024-01-15 12:42:42 +0200

    navbar150124

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './index.css'
-// import Hero from './components/home/Hero'
+import Hero from './components/home/Hero'
 // import ZX9Speaker from './components/home/ZX9Speaker'
 // import ZX7Speaker from './components/home/ZX7Speaker'
 // import YX1Earphones from './components/home/YX1Earphones'
 import Catalog from './components/shared/Catalog'
+import Header from './components/shared/Header'
+import Footer from './components/shared/Footer'
 
 const App = () => {
   return (
     <div>
-      {/* <Hero />
-      <ZX9Speaker />
+      <Header />
+      <Hero />
+      {/* <ZX9Speaker />
       <ZX7Speaker />
       <YX1Earphones /> */}
       <Catalog />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../styles/Header.css"
+import Navbar from "./Navbar";
+
+const Header = () => {
+    return (
+        <div className="header-main">
+
+        </div>
+    )
+}
+
+export default Header 
