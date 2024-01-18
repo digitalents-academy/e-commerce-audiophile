8d3f0d10b44d77668d78e740034a7cc20a5c0f69
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-18 09:46:52 +0200

    advert space done in desktop

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './index.css'
 // import Hero from './components/home/Hero'
 // import ZX9Speaker from './components/home/ZX9Speaker'
 // import ZX7Speaker from './components/home/ZX7Speaker'
-// import YX1Earphones from './components/home/YX1Earphones'
-import Catalog from './components/shared/Catalog'
+import YX1Earphones from './components/home/YX1Earphones'
+// import Catalog from './components/shared/Catalog'
+import Advert from './components/shared/Advert'
 
 const App = () => {
   return (
@@ -11,8 +12,9 @@ const App = () => {
       {/* <Hero />
       <ZX9Speaker />
       <ZX7Speaker />
-      <YX1Earphones /> */}
-      <Catalog />
+      */}
+      <YX1Earphones />
+      <Advert />
     </div>
   )
 }
---
+++ src/components/shared/Advert.jsx
@@ -0,0 +1,20 @@
+const Advert = () => {
+  return (
+    <div id="advert">
+      <div>
+        <h5>Bringing you the <span id="orange">best</span> audio gear</h5>
+        <p>
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </div>
+      <div id="advert-image"></div>
+    </div>
+  );
+};
+
+export default Advert;
