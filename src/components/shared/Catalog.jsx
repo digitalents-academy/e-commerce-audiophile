b3cf395ed407c60499bf4cc8efa84022f27f2702
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-22 13:36:40 +0200

    add links to catalog component

--- src/components/shared/Catalog.jsx
+++ src/components/shared/Catalog.jsx
@@ -1,24 +1,23 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Catalog = () => {
   return (
-    // TODO: figure out using links while keeping styling
     <div id="catalog">
-      <a href="/headphones">
+      <Link to={`/headphones`}>
         <img src="../assets/shared/desktop/image-headphones.png" alt="" className="catalog-image" />
         <h4>Headphones</h4>
         <div><span>Shop</span> <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" className="catalog-arrow" /></div>
-      </a>
-      <a href="/speakers">
+      </Link>
+      <Link to={`/speakers`}>
         <img src="../assets/shared/desktop/image-speakers.png" alt="" className="catalog-image" />
         <h4>Speakers</h4>
         <div><span>Shop</span> <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" className="catalog-arrow" /></div>
-      </a>
-      <a href="/earphones">
+      </Link>
+      <Link to={`/earphones`}>
         <img src="../assets/shared/desktop/image-earphones.png" alt="" className="catalog-image" />
         <h4>Earphones</h4>
         <div><span>Shop</span> <img src="../assets/shared/desktop/icon-arrow-right.svg" alt="" className="catalog-arrow" /></div>
-      </a>
+      </Link>
     </div>
   )
 };
