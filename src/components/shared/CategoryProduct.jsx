46eff0526445111ff21e409948c6e192deeae1c5
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-24 12:23:19 +0200

    add data to component from props, image source changes based on screen size

--- src/components/shared/CategoryProduct.jsx
+++ src/components/shared/CategoryProduct.jsx
@@ -1,6 +1,18 @@
-const CategoryProduct = () => {
+const CategoryProduct = ({ data, right }) => {
+
   return (
-    <div>Product</div>
+    <div>
+      <div>
+      {data.new ? "New Product" : ""}
+      {data.name} {data.category}
+      {data.description}
+      </div>
+      <picture>
+        <source media="(min-width:768px) and (max-width:1440px)" srcSet={"../../assets/category-" + data.category + "/tablet/" + data.image} />
+        <source media="(min-width: 1440px)" srcSet={"../../assets/category-" + data.category + "/desktop/" + data.image} />
+        <img src={"../../assets/category-" + data.category + "/mobile/" + data.image} alt={data.name + " " + data.category} />
+      </picture>
+    </div>
   )
 }
 
