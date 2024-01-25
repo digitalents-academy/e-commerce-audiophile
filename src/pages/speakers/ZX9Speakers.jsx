02a5c80dbaa668409de15a8fcf881c6a972bce00
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-25 11:56:14 +0200

    add data and page structure to zx9

--- src/pages/speakers/ZX9Speakers.jsx
+++ src/pages/speakers/ZX9Speakers.jsx
@@ -1,5 +1,82 @@
+import BackButton from "../../components/product-page/BackButton"
+import PhotoGrid from "../../components/product-page/PhotoGrid"
+import OtherProducts from "../../components/product-page/OtherProducts"
+import PhotoDetails from "../../components/product-page/PhotoDetails"
+import TextDetails from "../../components/product-page/TextDetails"
+
+import Catalog from "../../components/shared/Catalog"
+import Advert from "../../components/shared/Advert"
+
 const ZX9Speakers = () => {
-  return <div>Placeholder</div>
+  const data = {
+      imageFolder: "product-zx9-speaker",
+      name: "ZX9 Speaker",
+      new: true,
+      price: 4500,
+      description: "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+      features: [
+        "Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m).",
+        "Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms."
+      ],
+      box: [
+        {
+          quantity: 2,
+          item: "Speaker Unit"
+        },
+        {
+          quantity: 2,
+          item: "Speaker Cloth Panel"
+        },
+        {
+          quantity: 1,
+          item: "User Manual"
+        },
+        {
+          quantity: 1,
+          item: "3.5mm 10m Audio Cable"
+        },
+        {
+          quantity: 1,
+          item: "10m Optical Cable"
+        }
+      ],
+      others: [
+        {
+          name: "ZX7 Speaker",
+          url: "/speakers/zx7-speakers",
+          image: "image-zx7-speaker.jpg"
+        },
+        {
+          name: "XX99 Mark I",
+          url: "/headphones/xx99-mark-one-headphones",
+          image: "image-xx99-mark-two-headphones.jpg"
+        },
+        {
+          name: "XX59",
+          url: "/headphones/xx59-headphones",
+          image: "image-xx59-headphones.jpg"
+        }
+      ]
+  }
+
+
+  return (
+    <>
+      <div id="products" className="more-gap">
+        <BackButton />
+        <PhotoDetails imageFolder={data.imageFolder} name={data.name} isNew={data.new} price={data.price} description={data.description} />
+        <TextDetails features={data.features} box={data.box} />
+        <PhotoGrid imageFolder={data.imageFolder} />
+        <OtherProducts data={data.others} />
+      </div>
+      <div className='non-flex'>
+        <Catalog />
+      </div>
+      <div className='non-flex'>
+        <Advert />
+      </div>
+    </>
+  )
 }
 
 export default ZX9Speakers
