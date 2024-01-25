875aaf78e84c7e8257be1c01fde26ebf0351b902
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-25 10:26:55 +0200

    add data from props to text detail component

--- src/components/product-page/TextDetails.jsx
+++ src/components/product-page/TextDetails.jsx
@@ -1,5 +1,12 @@
-const TextDetails = () => {
-  return <div>Features & In The Box</div>
+const TextDetails = ({ features, box }) => {
+  return (
+    <div>
+      <p>{features}</p>
+      <ul>
+        {box.map(item => <li key={item.item}>{item.quantity}x {item.item}</li>)}
+      </ul>
+    </div>
+  )
 }
 
 export default TextDetails
--- src/pages/headphones/XX99Mark2.jsx
+++ src/pages/headphones/XX99Mark2.jsx
@@ -54,7 +54,7 @@ const XX99Mark2 = () => {
           image: "image-zx9-speaker.jpg"
         }
       ]
-    }
+  }
 
 
   return (
