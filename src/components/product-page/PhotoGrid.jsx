d15298de2592c45085fb0e72d3218d42ade07792
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-25 14:41:51 +0200

    photo grid done on desktop

--- src/components/product-page/PhotoGrid.jsx
+++ src/components/product-page/PhotoGrid.jsx
@@ -1,9 +1,21 @@
 const PhotoGrid = ({ imageFolder }) => {
   return (
-    <div>
-      <img src={"../../../assets/" + imageFolder + "/desktop/image-gallery-1.jpg"} alt="" />
-      <img src={"../../../assets/" + imageFolder + "/desktop/image-gallery-2.jpg"} alt="" />
-      <img src={"../../../assets/" + imageFolder + "/desktop/image-gallery-3.jpg"} alt="" />
+    <div className="photo-grid">
+      <picture className="item-one">
+        <source media="(min-width:768px) and (max-width:1440px)" srcSet={"../../../assets/" + imageFolder + "/tablet/image-gallery-1.jpg"} />
+        <source media="(min-width: 1440px)" srcSet={"../../../assets/" + imageFolder + "/desktop/image-gallery-1.jpg"} />
+        <img src={"../../../assets/" + imageFolder + "/mobile/image-gallery-1.jpg"} />
+      </picture>
+      <picture className="item-two">
+        <source media="(min-width:768px) and (max-width:1440px)" srcSet={"../../../assets/" + imageFolder + "/tablet/image-gallery-2.jpg"} />
+        <source media="(min-width: 1440px)" srcSet={"../../../assets/" + imageFolder + "/desktop/image-gallery-2.jpg"} />
+        <img src={"../../../assets/" + imageFolder + "/mobile/image-gallery-2.jpg"} />
+      </picture>
+      <picture className="item-three">
+        <source media="(min-width:768px) and (max-width:1440px)" srcSet={"../../../assets/" + imageFolder + "/tablet/image-gallery-3.jpg"} />
+        <source media="(min-width: 1440px)" srcSet={"../../../assets/" + imageFolder + "/desktop/image-gallery-3.jpg"} />
+        <img src={"../../../assets/" + imageFolder + "/mobile/image-gallery-3.jpg"} />
+      </picture>
     </div>
   )
 }
