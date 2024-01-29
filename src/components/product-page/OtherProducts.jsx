968b27b03cd6cdcd34fbdd0df666621435339730
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-29 10:21:19 +0200

    other products area done on desktop

--- src/components/product-page/OtherProducts.jsx
+++ src/components/product-page/OtherProducts.jsx
@@ -3,21 +3,35 @@ import { Link } from "react-router-dom"
 const OtherProducts = ({ data }) => {
   return (
     <div>
-      You May Also Like <br />
+      <h4 id="other-products">You May Also Like</h4>
+      <div className="product-flex">
       <div>
-        <img src={"../../../assets/shared/desktop/" + data[0].image} alt="" />
-        {data[0].name} <br />
+      <picture>
+        <source media="(min-width:768px) and (max-width:1440px)" srcSet={"../../../assets/shared/tablet/" + data[0].image} />
+        <source media="(min-width: 1440px)" srcSet={"../../../assets/shared/desktop/" + data[0].image} />
+        <img src={"../../../assets/shared/mobile/" + data[0].image} />
+      </picture>
+        <h5>{data[0].name}</h5>
         <Link to={data[0].url}>See Product</Link>
       </div>
       <div>
-        <img src={"../../../assets/shared/desktop/" + data[1].image} alt="" />
-        {data[1].name} <br />
+      <picture>
+        <source media="(min-width:768px) and (max-width:1440px)" srcSet={"../../../assets/shared/tablet/" + data[1].image} />
+        <source media="(min-width: 1440px)" srcSet={"../../../assets/shared/desktop/" + data[1].image} />
+        <img src={"../../../assets/shared/mobile/" + data[1].image} />
+      </picture>
+        <h5>{data[1].name}</h5>
         <Link to={data[1].url}>See Product</Link>
       </div>
       <div>
-        <img src={"../../../assets/shared/desktop/" + data[2].image} alt="" />
-        {data[2].name} <br />
+      <picture>
+        <source media="(min-width:768px) and (max-width:1440px)" srcSet={"../../../assets/shared/tablet/" + data[2].image} />
+        <source media="(min-width: 1440px)" srcSet={"../../../assets/shared/desktop/" + data[2].image} />
+        <img src={"../../../assets/shared/mobile/" + data[2].image} />
+      </picture>
+        <h5>{data[2].name}</h5>
         <Link to={data[2].url}>See Product</Link>
+      </div>
       </div>
     </div>
   )
